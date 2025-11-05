ada1fb67c708800392a4b1a750061c6a5c6d2aa7
Moulika Thammina <moulikat23@gmail.com>
2025-11-05 13:26:15 -0600

    Initial commit - tic tac toe game

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Board from "./Game";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="game">
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  );
+}
